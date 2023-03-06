c7e4b46afbcb168784ba2099344d4a6e3d279cad
Петров Никита <Petovn.xz@mail.ru>
2023-03-06 16:40:04 +0300

    create nav bar. add main content

--- Forum/Models/User.cs
+++ Forum/Models/User.cs
@@ -4,6 +4,7 @@ public class User
 {
     
     public int Id { get; set; }
+    public String NickName { get; set; }
     public String FirstName { get; set; }
     public String LastName { get; set; }
     public String Email { get; set; }
