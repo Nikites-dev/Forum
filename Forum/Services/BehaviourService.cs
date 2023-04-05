972cbab667d40cbd495743c66e4b246404e501f2
Nikites <rockstar.gg@mail.ru>
2023-04-05 18:37:03 +0300

    try change refresh data

--- Forum/Services/BehaviourService.cs
+++ Forum/Services/BehaviourService.cs
@@ -6,5 +6,10 @@ namespace Forum.Services
     {
         public bool isShowCommentContent { get; set; }
         public String SelectInterest { get; set; }
+        
+        public void OnFilterChange(string newValue)
+        {
+            SelectInterest = newValue;
+        }
     }
 }
