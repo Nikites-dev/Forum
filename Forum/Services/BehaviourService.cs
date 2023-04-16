4e847c37a3d6f091b8bdf1a2e951788b3ba75d31
Nikites <rockstar.gg@mail.ru>
2023-04-16 20:54:00 +0300

    add visibility sort menu

--- Forum/Services/BehaviourService.cs
+++ Forum/Services/BehaviourService.cs
@@ -5,6 +5,8 @@ namespace Forum.Services
     public class BehaviourService
     {
         public bool isShowCommentContent { get; set; }
+        public bool isProfile { get; set; }
+        public bool isShowSignComponent { get; set; }
         public String SelectInterest { get; set; }
         
         public void OnFilterChange(string newValue)
