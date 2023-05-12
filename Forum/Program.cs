cd49104060cde2d1a051438888a39c427fa8e291
Петров Никита <Petovn.xz@mail.ru>
2023-05-12 11:09:19 +0300

    add mud blazor

--- Forum/Program.cs
+++ Forum/Program.cs
@@ -9,6 +9,8 @@ using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using MudBlazor;
+using MudBlazor.Services;
 using Post = Forum.Models.Post;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -32,7 +34,22 @@ builder.Services.AddSingleton<PostDbConnection>();
 builder.Services.AddSingleton<CreateComment>();
 builder.Services.AddSingleton<Interests>();
 builder.Services.AddSingleton<ErrorMessage>();
+builder.Services.AddMudServices();
 // builder.Services.AddSingleton<CommentPost>();
+
+builder.Services.AddMudServices(config =>
+{
+    config.SnackbarConfiguration.PositionClass = Defaults.Classes.Position.BottomLeft;
+
+    config.SnackbarConfiguration.PreventDuplicates = false;
+    config.SnackbarConfiguration.NewestOnTop = false;
+    config.SnackbarConfiguration.ShowCloseIcon = true;
+    config.SnackbarConfiguration.VisibleStateDuration = 10000;
+    config.SnackbarConfiguration.HideTransitionDuration = 500;
+    config.SnackbarConfiguration.ShowTransitionDuration = 500;
+    config.SnackbarConfiguration.SnackbarVariant = Variant.Filled;
+});
+
 
 var app = builder.Build();
 
